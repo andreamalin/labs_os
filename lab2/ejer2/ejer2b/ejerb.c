abc8304065ff4f053b06c12cb975f8af408d7330
Andrea Amaya <andreaa.malin@gmail.com>
2022-02-27 11:51:06 -0600

    ejer2 terminado

--- lab2/ejer2/ejer2b/ejerb.c
+++ lab2/ejer2/ejer2b/ejerb.c
@@ -7,36 +7,37 @@ int main(int argc, char const *argv[])
 {
     pid_t fork1, fork2, fork3;
     clock_t start, end;
+    double time;
     int million = 1000000;
 
-    start = clock()
+    start = clock();
     
     // hijo
-    fork1 = fork()
+    fork1 = fork();
 
     if (fork1 == 0) {
         // 2do hijo
-        fork2 = fork()
+        fork2 = fork();
         if (fork2 == 0) {
             // 3er hijo
-            fork3 = fork()
+            fork3 = fork();
             if (fork3 == 0) {
                 for (int i = 0; i < million; i++) {}
             } else {
                 for (int i = 0; i < million; i++) {}
-                wait(NULL)
+                wait(NULL);
             } 
         } else {
             for (int i = 0; i < million; i++) {}
-            wait(NULL)
+            wait(NULL);
         }
     } else {
-        wait(NULL)
-        end = clock()
-    }
+        wait(NULL);
 
-    time = (double)(end - start);
-    printf("El tiempo final es de %f ms\n", time);
+        end = clock();
+        time = (double)(end - start);
+        printf("El tiempo final es de %f ms\n", time);
+    }
 
     return 0;
 }
