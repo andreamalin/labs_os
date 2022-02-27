86b097344d17a4233a091364e6b991767b686bc6
Andrea Amaya <andreaa.malin@gmail.com>
2022-02-27 11:43:25 -0600

    ejer2b

--- ejerb.c
+++ ejerb.c
@@ -13,21 +13,30 @@ int main(int argc, char const *argv[])
     
     // hijo
     fork1 = fork()
-    // 2do hijo
-    fork2 = fork()
-    // 3er hijo
-    fork3 = fork()
 
-    if (fork1 == 0 && fork2 !== 0 && fork3 !== 0) {
-        for (int i = 0; i < million; i++) {}
-    } else if (fork1 !== 0 && fork2 == 0 && fork3 !== 0) {
-        for (int i = 0; i < million; i++) {}
-    } else if (fork1 !== 0 && fork2 !== 0 && fork3 == 0) {
-        for (int i = 0; i < million; i++) {}
+    if (fork1 == 0) {
+        // 2do hijo
+        fork2 = fork()
+        if (fork2 == 0) {
+            // 3er hijo
+            fork3 = fork()
+            if (fork3 == 0) {
+                for (int i = 0; i < million; i++) {}
+            } else {
+                for (int i = 0; i < million; i++) {}
+                wait(NULL)
+            } 
+        } else {
+            for (int i = 0; i < million; i++) {}
+            wait(NULL)
+        }
     } else {
         wait(NULL)
+        end = clock()
     }
-    
+
+    time = (double)(end - start);
+    printf("El tiempo final es de %f ms\n", time);
 
     return 0;
 }
