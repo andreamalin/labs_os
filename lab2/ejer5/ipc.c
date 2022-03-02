66045f4f31469914434921f14b3507fb56d53ddf
Andrea Amaya <andreaa.malin@gmail.com>
2022-03-02 14:41:03 -0600

    Pipe

--- lab2/ejer5/ipc.c
+++ lab2/ejer5/ipc.c
@@ -1,70 +1,78 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char*argv[])
 {
 
     // Progra defensiva
     if (argc < 3) {
         printf("Faltan argumentos\n");
-        exit(1);
+        return 1;
     }
 
     // Obteniendo los argumentos
-    int n = (int)argv[1];
-    const char *x = argv[2];
+    int n = strtol(argv[1], NULL, 0);
     // Shared memory
-    int SIZE = 20;
+    int SIZE = 10;
     int currentPos = 0;
     // Pipes
     int b[2];
     pipe(b);
-
     // File descriptor
-    int fd = shm_open("file1.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1)
+    int fd = open("./file1.txt", O_RDWR, S_IRUSR | S_IWUSR);
+    struct stat sb;
+    if (fstat(fd, &sb) == -1)
     {
         printf("Error producido en file descriptor %s\n", strerror(errno));
-        exit(1);
-    } else {
-        printf("%d\n", fd);
-    }
 
+        return 1;
+    }
     // Shared memory
-    char *shared_memory = mmap(NULL, SIZE*sizeof(int), PROT_READ | PROT_WRITE,
-                                    MAP_SHARED | MAP_ANONYMOUS,
+    char *shared_memory = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE,
+                                    MAP_SHARED,
                                     fd, 0 );
 
     // 
     int f1 = fork();
     if (f1 == 0) {
         // Hijo
-        close(b[1]); // Cerramos escritura
-        // Si es valor 1, significa que escribimos en la memoria
-        if (read(b[1], 1, 1) == 1) {
-            shared_memory[currentPos] = x;
-            currentPos++;
+        
+        while(1) {
+            int x = 0;
+            read(b[0], (char*)&x, sizeof(int));
+
+            // Si es valor 1, significa que escribimos en la memoria
+            if (x == 1) {
+                shared_memory[currentPos] = argv[2];
+                currentPos++;
+            }
+            sleep(1);
         }
+
+        close(b[1]); // Cerramos escritura
     } else {
-        wait(NULL);
         // Padre
         for (int i = 0; i < SIZE; i++)
         {
-            close(b[0]); // Cerramos lectura
-
+            int value = 0;
             if (i % n == 0) {
-                write(b[1], 1, 1); // Escribimos valor 1
-            } else {
-                write(b[1], 0, 1); // Escribimos valor 0
+                // Escribimos valor 1
+                value = 1;
             }
-            
-            close(b[1]); // Cerramos escritura
+    
+            write(b[1], (char*)&value, sizeof(int));
+            sleep(2);
         }
-        
+        printf("Memoria compartida: %s\n", shared_memory);
     }
 
     return 0;
