9e229b08a5698b4f5b34e72d7cf33d45cf8c1a93
Andrea Amaya <andreaa.malin@gmail.com>
2022-03-01 23:10:29 -0600

    pipe ejer5 lab2

--- lab2/ejer5/ipc.c
+++ lab2/ejer5/ipc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
@@ -16,41 +19,50 @@ int main(int argc, char const *argv[])
     const char *x = argv[2];
     // Shared memory
     int SIZE = 20;
-    int updateSharedMemory = 0;
     int currentPos = 0;
+    // Pipes
+    int b[2];
+    pipe(b);
 
-    // Obteniendo file descriptor
-    int fd = open("file1.txt", O_RDONLY);
+    // File descriptor
+    int fd = shm_open("file1.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd == -1)
     {
-        printf("File doesn't exists\n");
+        printf("Error producido en file descriptor %s\n", strerror(errno));
+        exit(1);
     } else {
         printf("%d\n", fd);
     }
 
-    // Abrir espacio de memoria
-    int *shared_memory = mmap(NULL, SIZE*sizeof(int), PROT_READ | PROT_WRITE,
+    // Shared memory
+    char *shared_memory = mmap(NULL, SIZE*sizeof(int), PROT_READ | PROT_WRITE,
                                     MAP_SHARED | MAP_ANONYMOUS,
-                                    0, 0 );
-
-    close(fd);
+                                    fd, 0 );
 
     // 
     int f1 = fork();
     if (f1 == 0) {
         // Hijo
-        if (updateSharedMemory == 1) {
+        close(b[1]); // Cerramos escritura
+        // Si es valor 1, significa que escribimos en la memoria
+        if (read(b[1], 1, 1) == 1) {
             shared_memory[currentPos] = x;
             currentPos++;
-            updateSharedMemory = 0;
         }
     } else {
+        wait(NULL);
         // Padre
         for (int i = 0; i < SIZE; i++)
         {
+            close(b[0]); // Cerramos lectura
+
             if (i % n == 0) {
-                updateSharedMemory = 1;
+                write(b[1], 1, 1); // Escribimos valor 1
+            } else {
+                write(b[1], 0, 1); // Escribimos valor 0
             }
+            
+            close(b[1]); // Cerramos escritura
         }
         
     }
