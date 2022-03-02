633308bb9fd08a0e0cb1a760294f53aceb9a5bed
Andrea Amaya <andreaa.malin@gmail.com>
2022-03-01 21:00:52 -0600

    Inicio ejer5 lab2

---
+++ lab2/ejer5/ipc.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main(int argc, char const *argv[])
+{
+    // Obteniendo los argumentos
+    int n = (int)argv[0];
+    const char *x = argv[1];
+    // Shared memory
+    int SIZE = 20;
+    int updateSharedMemory = 0;
+    int currentPos = 0;
+
+    // Obteniendo file descriptor
+    int fd = open("file1.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        printf("File doesn't exists\n");
+    } else {
+        printf("%d\n", fd);
+    }
+
+    // Abrir espacio de memoria
+    int *shared_memory = mmap(NULL, SIZE*sizeof(int), PROT_READ | PROT_WRITE,
+                                    MAP_SHARED | MAP_ANONYMOUS,
+                                    0, 0 );
+
+    close(fd);
+
+    // 
+    int f1 = fork();
+    if (f1 == 0) {
+        // Hijo
+        if (updateSharedMemory == 1) {
+            shared_memory[currentPos] = x;
+            currentPos++;
+        }
+    } else {
+        // Padre
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (i % n == 0) {
+                updateSharedMemory = 1;
+            }
+        }
+        
+    }
+
+    return 0;
+}
